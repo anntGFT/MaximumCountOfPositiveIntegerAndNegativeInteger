08746709477fb3e71e4487c4cfb4bdecd31c96b6
annt <annt@gft.com>
2023-01-09 10:33:14 +0000

    done

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,26 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution() {
+
+    }
+
+    public int maximumCount(int[] nums) {
+
+        int contPos = 0;
+        int contNeg = 0;
+
+        for (int num : nums) {
+
+            if (num > 0) {
+
+                contPos++;
+            } else if (num < 0) {
+
+                contNeg++;
+            }
+        }
+
+        return Math.max(contPos, contNeg);
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,65 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    int result;
+    int[] nums;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void emptyArray(){
+
+        result = 0;
+        nums = new int[] {};
+
+        assertEquals(result, solution.maximumCount(nums));
+    }
+
+    @Test
+    public void arrayWithOnlyZeros(){
+
+        result = 0;
+        nums = new int[] {0,0,0};
+
+        assertEquals(result, solution.maximumCount(nums));
+    }
+
+    @Test
+    public void arrayWithOnlyPositives(){
+
+        result = 3;
+        nums = new int[] {1,3,44};
+
+        assertEquals(result, solution.maximumCount(nums));
+    }
+
+    @Test
+    public void arrayWithOnlyNegatives(){
+
+        result = 3;
+        nums = new int[] {-1,-3,-44};
+
+        assertEquals(result, solution.maximumCount(nums));
+    }
+
+    @Test
+    public void arrayWithMoreNegativesThanPositives(){
+
+        result = 2;
+        nums = new int[] {-1,-3,44};
+
+        assertEquals(result, solution.maximumCount(nums));
+    }
 }
